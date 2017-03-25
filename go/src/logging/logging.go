51ba137944089a501c75236c589c735264e38c0e
Chao Liu <yijinliu@gmail.com>
2017-03-25 00:21:09 -0700

    Add more logging colors.

--- go/src/logging/logging.go
+++ go/src/logging/logging.go
@@ -6,53 +6,53 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"path"
 	"runtime"
+	"strings"
+)
+
+const (
+	COLOR_RED    = "\033[0;31m"
+	COLOR_YELLOW = "\033[0;33m"
+	COLOR_GREEN  = "\033[0;32m"
+	COLOR_NONE   = "\033[0m"
 )
 
 var vFlag = flag.Int("v", 1, "")
 
-func getFileLinePrefix() string {
-	if _, file, line, ok := runtime.Caller(2); ok {
-		return fmt.Sprintf("[%s:%d] ", path.Base(file), line)
-	}
-	return ""
-}
-
 func Fatal(v ...interface{}) {
-	log.Fatal(getFileLinePrefix(), fmt.Sprint(v...))
+	log.Fatal(fileLinePrefix(), fmt.Sprint(v...))
 }
 
 func Fatalf(format string, v ...interface{}) {
-	log.Fatal(getFileLinePrefix(), fmt.Sprintf(format, v...))
+	log.Fatal(fileLinePrefix(), fmt.Sprintf(format, v...))
 }
 
 func Fatalln(v ...interface{}) {
-	log.Fatalln(getFileLinePrefix(), fmt.Sprint(v...))
+	log.Fatalln(fileLinePrefix(), fmt.Sprint(v...))
 }
 
 func Panic(v ...interface{}) {
-	log.Panic(getFileLinePrefix(), fmt.Sprint(v...))
+	log.Panic(fileLinePrefix(), fmt.Sprint(v...))
 }
 
 func Panicf(format string, v ...interface{}) {
-	log.Panic(getFileLinePrefix(), fmt.Sprintf(format, v...))
+	log.Panic(fileLinePrefix(), fmt.Sprintf(format, v...))
 }
 
 func Panicln(v ...interface{}) {
-	log.Panicln(getFileLinePrefix(), fmt.Sprint(v...))
+	log.Panicln(fileLinePrefix(), fmt.Sprint(v...))
 }
 
 func Print(v ...interface{}) {
-	log.Print(getFileLinePrefix(), fmt.Sprint(v...))
+	log.Print(fileLinePrefix(), fmt.Sprint(v...))
 }
 
 func Printf(format string, v ...interface{}) {
-	log.Print(getFileLinePrefix(), fmt.Sprintf(format, v...))
+	log.Print(fileLinePrefix(), fmt.Sprintf(format, v...))
 }
 
 func Println(v ...interface{}) {
-	log.Println(getFileLinePrefix(), fmt.Sprint(v...))
+	log.Println(fileLinePrefix(), fmt.Sprint(v...))
 }
 
 func GetVerboseLevel() int {
@@ -63,30 +63,56 @@ func SetVerboseLevel(level int) {
 	*vFlag = level
 }
 
-func getVlogPrefix(level int) string {
-	if level < 0 {
-		return "\033[0;31m"
-	} else if level == 0 {
-		return "\033[0;33m"
-	}
-	return ""
-}
-
-func getVlogSuffix(level int) string {
-	if level <= 0 {
-		return "\033[0m"
-	}
-	return ""
-}
-
 func Vlog(level int, v ...interface{}) {
 	if level <= *vFlag {
-		log.Print(getFileLinePrefix(), getVlogPrefix(level), fmt.Sprint(v...), getVlogSuffix(level))
+		log.Print(fileLinePrefix(), vlogPrefix(level), fmt.Sprint(v...), vlogSuffix(level))
 	}
 }
 
 func Vlogf(level int, format string, v ...interface{}) {
 	if level <= *vFlag {
-		log.Print(getFileLinePrefix(), getVlogPrefix(level), fmt.Sprintf(format, v...), getVlogSuffix(level))
+		log.Print(fileLinePrefix(), vlogPrefix(level), fmt.Sprintf(format, v...), vlogSuffix(level))
 	}
+}
+
+func vlogPrefix(level int) string {
+	if level < 0 {
+		return COLOR_RED
+	} else if level == 0 {
+		return COLOR_YELLOW
+	} else if level >= 3 {
+		return COLOR_GREEN
+	}
+	return ""
+}
+
+func vlogSuffix(level int) string {
+	if level <= 0 || level >= 3 {
+		return COLOR_NONE
+	}
+	return ""
+}
+
+func fileLinePrefix() string {
+	// Find the first caller outside of package logging.
+	for i := 2; ; i++ {
+		if _, file, line, ok := runtime.Caller(i); ok {
+			file = srcFilePath(file)
+			if strings.Contains(file, "/logging/") {
+				continue
+			}
+			return fmt.Sprintf("[%s:%d] ", file, line)
+		}
+		break
+	}
+	return "[unknown] "
+}
+
+// Returns the source file name after "go/src/".
+func srcFilePath(path string) string {
+	const START_AFTER = "go/src/"
+	if index := strings.LastIndex(path, START_AFTER); index != -1 {
+		return path[index+len(START_AFTER):]
+	}
+	return path
 }
